9afc752bab0041fa35ac7802ef5062e43a7f3b6e
Tristan Derez <tristan.derez@gmail.com>
2024-03-21 13:41:45 +0100

    create function to crawl pages

--- crawl.js
+++ crawl.js
@@ -1,6 +1,6 @@
 import { JSDOM } from "jsdom";
 
-export function normalizeURL(url) {
+export const normalizeURL = (url) => {
   const urlObj = new URL(url);
   let fullPath = `${urlObj.host}${urlObj.pathname}`;
 
@@ -9,9 +9,9 @@ export function normalizeURL(url) {
   }
 
   return fullPath;
-}
+};
 
-export function getURLsFromHTML(htmlBody, baseURL) {
+export const getURLsFromHTML = (htmlBody, baseURL) => {
   const dom = new JSDOM(htmlBody, { url: baseURL });
   const links = Array.from(dom.window.document.querySelectorAll("a"));
   const urls = [];
@@ -25,4 +25,50 @@ export function getURLsFromHTML(htmlBody, baseURL) {
   });
 
   return urls;
-}
+};
+
+export const crawlPage = async (baseURL, currentURL, pages) => {
+  const currentHost = new URL(currentURL);
+  const baseHost = new URL(baseURL);
+
+  if (currentHost.hostname !== baseHost.hostname) {
+    return pages;
+  }
+
+  const normalizedURL = normalizeURL(currentURL);
+
+  if (pages[normalizedURL] > 0) {
+    pages[normalizedURL]++;
+    return pages;
+  }
+
+  pages[normalizedURL] = 1;
+
+  console.log(`crawling ${currentURL}`);
+
+  try {
+    const response = await fetch(currentURL);
+    const contentType = response.headers.get("content-type");
+
+    if (response.status >= 400) {
+      console.error(`Error ${response.status}: ${response.statusText}`);
+      return pages;
+    }
+
+    if (contentType && !contentType.includes("text/html")) {
+      console.error("Error: Content-type is not text/html");
+      return pages;
+    }
+
+    const htmlText = await response.text();
+    const urlsToParse = getURLsFromHTML(htmlText, currentURL);
+
+    for (const nextURL of urlsToParse) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
+
+    return pages;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
