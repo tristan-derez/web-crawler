4ca7d5357e32218f708b84e631278fe38f522676
Tristan Derez <tristan.derez@gmail.com>
2024-03-21 13:42:04 +0100

    create main function to call crawlpages and generate report

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+import { crawlPage } from "./crawl.js";
+import { printReport } from "./report.js";
+
+const main = async () => {
+  const args = process.argv.slice(2);
+
+  if (args.length !== 1) {
+    console.error("Error: Provide exactly one argument (an URL).");
+    process.exit(1);
+  }
+
+  const baseURL = args[0];
+
+  try {
+    console.log(`Starting crawler at ${baseURL}`);
+
+    const pages = await crawlPage(baseURL, baseURL, {});
+
+    printReport(pages);
+  } catch (error) {
+    console.error("Error: The provided argument is not a valid URL.");
+    process.exit(1);
+  }
+};
+
+main();
